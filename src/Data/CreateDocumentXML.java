88b641bebfd5d70e784e1c80a785d7ebe7589f73
andreia machado <andreiarfmachado4@gmail.com>
2016-10-19 22:36:14 +0100

    Update docs and from to insert docs
    Todos os documentos que devem estar armazenados na DB e fiz um form para
    cria o documento mas por enquanto nao adiciona a DB

---
+++ src/Data/CreateDocumentXML.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Data;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
+/**
+ *
+ * @author Andreia Machado
+ */
+public class CreateDocumentXML {
+    private String nome;
+    private String conteudo;
+    
+    public CreateDocumentXML(String nome) {
+        this.nome = nome;
+        this.conteudo = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n";
+    }
+    
+    public void createDocument(int id, String preparacao, String cozinha, String dificuldade, String tempo, String dose) throws IOException {
+        putTagPrato("inicio");
+        putTagID(id);
+        putTagPreparacao(preparacao);
+        putTagCozinha(cozinha);
+        putTagDificuldade(dificuldade);
+        putTagTempo(tempo);
+        putTagDose(dose);
+        putTagPrato("fim");
+        createFile();
+    }
+
+    private void putTagPrato(String tag) {
+        if (tag.equals("inicio"))
+            conteudo +="<prato>\n";
+        else
+            conteudo +="</prato>\n"; 
+    }
+
+    private void putTagID(int id) {
+        conteudo+="\t<id>"+id+"</id>\n";
+    }
+
+    private void putTagPreparacao(String preparacao) {
+        conteudo+="\t<preparacao>"+preparacao+"</preparacao>\n";
+    }
+
+    private void putTagCozinha(String cozinha) {
+        conteudo+="\t<Cozinha>"+cozinha+"</Cozinha>\n";
+    }
+
+    private void putTagDificuldade(String dificuldade) {
+        conteudo+="\t<Dificuldade>"+dificuldade+"</Dificuladade>\n";
+    }
+
+    private void putTagTempo(String tempo) {
+        conteudo+="\t<Tempo>"+tempo+"</Tempo>\n";
+    }
+
+    private void putTagDose(String dose) {
+        conteudo+="\t<Dose>"+dose+"</Dose>\n";
+    }  
+
+    private void createFile() throws IOException {
+        try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("res\\doc\\" + nome + ".xml"), "utf-8"))) {
+            writer.write(conteudo);
+        }
+    }
+    
+}
