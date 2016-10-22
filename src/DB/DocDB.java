1316f6ef1e5d0e60d6ce20045f1ebbc3c8fb539d
andreia machado <andreiarfmachado4@gmail.com>
2016-10-22 01:01:34 +0100

    Imports fixed

--- src/DB/DocDB.java
+++ src/DB/DocDB.java
@@ -5,9 +5,13 @@
  */
 package DB;
 
+import Data.CreateDocumentXML;
 import Data.Prato;
+import cbd_nutribd.InsertDoc;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -16,6 +20,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
